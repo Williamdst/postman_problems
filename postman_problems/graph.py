5cb2e1c2bd10df2c25bc18d9154e7f577d6dfd5a
Denzel S. Williams <49127460+Williamdst@users.noreply.github.com>
2021-04-22 20:09:15 -0400

    Added Eulerian Path
    Copied the create_eulerian_circuit function to a function called create_eulerian_path. In this function I simply replaced the call to nx.eulerian_circuit with a call to nx.eulerian_path. The variable that saves the output is still called euler_circuit.

--- postman_problems/graph.py
+++ postman_problems/graph.py
@@ -214,7 +214,36 @@ def create_eulerian_circuit(graph_augmented, graph_original, start_node=None):
                 edge_aug_shortest['id'] = edge_aug_dict[edge_key]['id']
                 yield edge_aug + (edge_key, edge_aug_shortest, )
 
+def create_eulerian_path(graph_augmented, graph_original, start_node=None):
+    """
+    Args:
+    graph_augmented (networkx graph): graph w links between odd degree nodes created from `add_augmenting_path_to_graph`.
+    graph_original (networkx graph): orginal graph created from `create_networkx_graph_from_edgelist`
+    start_node (str): name of starting (and ending) node for CPP solution.
 
+    Returns:
+        networkx graph (`graph_original`) augmented with edges directly between the odd nodes
+    """
+
+    euler_circuit = list(nx.eulerian_path(graph_augmented, source=start_node, keys=True))
+    assert len(graph_augmented.edges()) == len(euler_circuit), 'graph and euler_circuit do not have equal number of edges.'
+
+    for edge in euler_circuit:
+        aug_path = nx.shortest_path(graph_original, edge[0], edge[1], weight='distance')
+        edge_attr = graph_augmented[edge[0]][edge[1]][edge[2]]
+        if not edge_attr.get('augmented'):
+            yield edge + (edge_attr,)
+        else:
+            for edge_aug in list(zip(aug_path[:-1], aug_path[1:])):
+                # find edge with shortest distance (if there are two parallel edges between the same nodes)
+                edge_aug_dict = graph_original[edge_aug[0]][edge_aug[1]]
+                edge_key = min(edge_aug_dict.keys(), key=(lambda k: edge_aug_dict[k]['distance']))  # index with min distance
+                edge_aug_shortest = edge_aug_dict[edge_key]
+                edge_aug_shortest['augmented'] = True
+                edge_aug_shortest['id'] = edge_aug_dict[edge_key]['id']
+                yield edge_aug + (edge_key, edge_aug_shortest, )
+                
+                
 def create_required_graph(graph):
     """
     Strip a graph down to just the required nodes and edges.  Used for RPP.  Expected edge attribute "required" with
